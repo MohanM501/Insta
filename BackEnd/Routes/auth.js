678417c3792db466926d8a79a052b968412d6b6d
MohanM501 <mohananna501b@gmail.com>
2023-03-22 22:52:13 +0530

    ch

---
+++ BackEnd/Routes/auth.js
@@ -0,0 +1,13 @@
+const {Router}=require('express');
+
+const authRouter=Router();
+
+authRouter.post("/signup",(req,res)=>{
+
+})
+
+authRouter.post("/login",(req,res)=>{
+
+})
+
+module.exports={authRouter}
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,1 +1,14 @@
-//hi
+const express=require("express");
+const { authRouter } = require("./Routes/auth");
+
+const app=express();
+
+app.use(express.urlencoded({extended :true}));
+app.use(express.json());
+
+app.use("/auth",authRouter)
+
+
+app.listen(8080, ()=>{
+    console.log("listening on port 8080")
+})
